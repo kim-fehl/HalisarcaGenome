ade88e28ba390bc56c8e76618cb472eb0de94c93
Vasiliy Zubarev <123082132+VasiliyZubarev@users.noreply.github.com>
2025-12-21 17:20:17 +0300

    Create run_3d_dna.sh

---
+++ 3_scaffold/run_3d_dna.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --partition=compute
+#SBATCH --job-name=3D-DNA
+#SBATCH --mem=150G
+#SBATCH --cpus-per-task=24
+#SBATCH --time=5-00:00:00
+#SBATCH --output=/gpfs/khrameeva/Halisarca_analysis/2025_11_08_hic_scaffolding/3d-dna/batch1_lowcoarseres/3d-dna.log
+#SBATCH --error=/gpfs/khrameeva/Halisarca_analysis/2025_11_08_hic_scaffolding/3d-dna/batch1_lowcoarseres/3d-dna.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=Vasiliy.Zubarev@skoltech.ru
+
+source /home/khrameeva/software/miniconda3/bin/activate /home/khrameeva/software/juicer
+export PATH=${PATH}:/home/khrameeva/software/3d-dna/
+
+bash run-asm-pipeline.sh \
+	--mapq 30 \
+	--editor-fine-resolution 1000 \
+	--editor-coarse-resolution 50000 \
+	--editor-coarse-region 250000 \
+	--editor-repeat-coverage 40 \
+	--mode diploid \
+	/gpfs/khrameeva/Halisarca_analysis/2025_10_08_Nanopore_longest_assembly/1_RESULTS/Nextdenovo_60x_pilon_purgedups_nomito_nobact.fasta \
+	/gpfs/khrameeva/Halisarca_analysis/2025_11_08_hic_scaffolding/juicer_batch1/aligned/merged_nodups.txt
+
+
